171aa58ea5c7e216e4b4bd33ff3b3d1726233e73
robng725 <robng725@liu.student.se>
2023-06-20 17:45:10 +0200

    Add login and logout constants

--- master_planner/master_planner/settings.py
+++ master_planner/master_planner/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'master_planner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+                BASE_DIR / "Templates"
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL =  "/home"
+LOGOUT_REDIRECT_URL =  "/account/login"
