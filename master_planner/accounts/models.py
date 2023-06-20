e1acd963b35548a29460af8698235bdca0b7709d
robng725 <robng725@liu.student.se>
2023-06-20 17:43:30 +0200

    Fix models

--- master_planner/accounts/models.py
+++ master_planner/accounts/models.py
@@ -3,7 +3,14 @@ from django.contrib.auth.models import User
 from planning.models import Program, Schedule
 
 class Account(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
-    program_code = models.ForeignKey(Program, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    program_code = models.ForeignKey(Program, on_delete=models.CASCADE, blank=True, null=True)
     choices = models.ManyToManyField(Schedule, blank=True)
+
+def register_account(username: str, email: str, password: str):
+    user = User.objects.create_user(username=username, password=password, email=email)
+    account = Account(user=user)
+    
+    account.save()
+    return account
     
