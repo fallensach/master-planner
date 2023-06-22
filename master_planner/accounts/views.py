79f3e16a1e362dd8bab7aee01d702ba8cca7dad5
robng725 <robng725@liu.student.se>
2023-06-22 12:29:04 +0200

    Add registration and login
    Added views for registration and login. Also functionality to properly
    register and login. Registration now also adds user to the database.

--- master_planner/accounts/views.py
+++ master_planner/accounts/views.py
@@ -1,48 +1,29 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from scrappy.program_plan import ProgramPlan
-from django.contrib.auth import authenticate
-from .forms import RegisterForm, ProgramForm, ProfileCourses
-from accounts.models import Account, register_account
-from planning.models import register_program, Profile
-
-def index(request):
-    return HttpResponse()
-
-def logged_in(request):
-    if not request.user.is_authenticated:
-        return redirect("/account/login")
-    return HttpResponse("logged in")
-
-def home(request):
-    if request.method == "POST":
-        form = ProgramForm(request.POST)
-        form2 = ProfileCourses(request.POST)
-        if form.is_valid():
-            program_code = form.cleaned_data["program"]
-            register_program(program_code)
-        
-        if form2.is_valid():
-            profile_code = form2.cleaned_data["profile"]
-            x = Profile.objects.get(profile_code=profile_code)
-            print(x)
-    
-    else:
-        form = ProgramForm()
-        form2 = ProfileCourses(request.POST)
-
-    return render(request, "home.html", {"form": form, "form2": form2})
+from .forms import RegisterForm
+from accounts.models import register_account
+from django.contrib.auth import authenticate, login
     
 def register(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    success = True
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data["username"]
-            password = form.cleaned_data["password"]
-            email = form.cleaned_data["email"]
-            register_account(username, email, password)
-            return redirect("account/login")
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            email = request.POST.get("email")
+            success = register_account(username, email, password)
+            
+            if success:
+                user = authenticate(request, username=username, password=password)
+                login(request, user)
+                return redirect("home")
+            else:
+                form = RegisterForm()
+                return render(request, "registration/register.html", {"form": form, "success": success})
     else:
         form = RegisterForm()
     
-    return render(request, "registration/register.html", {"form": form})
+    return render(request, "registration/register.html", {"form": form, "success": success})
