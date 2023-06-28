7bd528fc9bc06ddd5ee4501256d26408cf45636a
robin <drken_robin@hotmail.com>
2023-06-28 09:46:55 +0200

    Add daisyui
    Added support for daisyui

--- master_planner/theme/static_src/tailwind.config.js
+++ master_planner/theme/static_src/tailwind.config.js
@@ -63,5 +63,6 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require('daisyui'),
     ],
 }
