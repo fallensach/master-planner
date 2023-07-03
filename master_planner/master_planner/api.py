a8e77458579d022138d0b5298b09385d3031ec73
robin <drken_robin@hotmail.com>
2023-07-03 16:33:59 +0200

    Added api url for getting term courses

--- master_planner/master_planner/api.py
+++ master_planner/master_planner/api.py
@@ -1,7 +1,9 @@
 from ninja import NinjaAPI, ModelSchema
 from django.http.response import JsonResponse
-from planning.models import Schedule, Course, Scheduler
+from planning.models import Schedule, Course, Scheduler, get_courses_term
 from planning.management.commands.scrappy.courses import fetch_course_info
+from accounts.models import get_user, Account
+from typing import List, Dict
 
 api = NinjaAPI()
 
@@ -28,17 +30,39 @@ def get_schedule(request, schedule_id):
 @api.get('get_course/{course_code}', response=CourseSchema)
 def get_course(request, course_code):
     course_info = Course.objects.get(course_code=course_code)
-    
     return course_info
 
-# @api.get('')
+@api.get('get_courses/{profile}/{semester}')
+def get_semester_courses(request, profile, semester):
+    program = Account.objects.get(user__username=request.user).program
+    
+#    period1 = Scheduler.objects.filter(program=program, 
+#                                       profile=profile, 
+#                                       schedule__semester=f"Termin {semester}",
+#                                       schedule__period="Period 1")
+#    period2 = Scheduler.objects.filter(program=program, 
+#                                       profile=profile, 
+#                                       schedule__semester=f"Termin {semester}",
+#                                       schedule__period="Period 2")
+    
+    data = get_courses_term(program, f"Termin {semester}", profile)
+
+    """    
+    print(period1)
+
+    data = {"period 1": period1,
+            "period 2": period2}
+    """
+    return data
+
 
 @api.get('get_extra_course_info/{course_code}')
 def get_extra_course_info(request, course_code):
-    # extra_info = fetch_course_info(course_code)
-    # course = Course.objects.get(course_code=course_code)
-    # extra_info["course_code"] = course.course_code
-    # extra_info["course_name"] = course.course_name
+    extra_info = fetch_course_info(course_code)
+    course = Course.objects.get(course_code=course_code)
+    extra_info["course_code"] = course.course_code
+    extra_info["course_name"] = course.course_name
+    """    
     return  {"examination": [{"code": "lab",
                               "name":  "laboration",
                               "scope": "6",
@@ -46,5 +70,5 @@ def get_extra_course_info(request, course_code):
              "examinator": "cyrille",
              "location": "valla",
              "main_field": ["matematik"]
-                }
-    # return extra_info
+                }"""
+    return extra_info
