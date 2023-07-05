116111e25ff7aa63cc92b79e203313aa2cc9251d
adsamu <samuelson93@gmail.com>
2023-07-05 18:50:57 +0200

    moved around in build

--- master_planner/build.sh
+++ master_planner/build.sh
@@ -4,7 +4,7 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
+python manage.py tailwind build
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py tailwind build
