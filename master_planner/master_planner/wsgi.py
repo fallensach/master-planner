6eb049768ee44a04f656fbb2559b9eb294edb35b
adsamu <samuelson93@gmail.com>
2023-07-05 18:05:51 +0200

    fixing deployment bugs

--- master_planner/master_planner/wsgi.py
+++ master_planner/master_planner/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'master_planner.master_planner.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'master_planner.settings')
 
 application = get_wsgi_application()
