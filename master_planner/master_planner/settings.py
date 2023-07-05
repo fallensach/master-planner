427eccf339735315174b3ec762864b24ea1fcf93
adsamu <samuelson93@gmail.com>
2023-07-05 17:10:20 +0200

    added dependencies to requirments.txt that are needed for testing deploymentand changed settings in setting.py

--- master_planner/master_planner/settings.py
+++ master_planner/master_planner/settings.py
@@ -12,21 +12,34 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from sys import platform
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
+#deploy
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$ae^hek6-$&d1i(64@2876p$l!5hsw#y0gqobn5#-ti#4ns^=s'
+#SECRET_KEY = 'django-insecure-$ae^hek6-$&d1i(64@2876p$l!5hsw#y0gqobn5#-ti#4ns^=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+
+                                             
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -82,13 +95,17 @@ WSGI_APPLICATION = 'master_planner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
