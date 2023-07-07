53e40b3fe97f67f51dc37ff7ed6adc5c1a01b6c7
Robin Ngo <drken_robin@hotmail.com>
2023-07-07 11:53:09 +0200

    Update build.sh
    add tailwind again

--- master_planner/build.sh
+++ master_planner/build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 set -o errexit  # exit on error
+cd theme/static_src
+npm i --production
+cd ../..
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+python manage.py populate_db
