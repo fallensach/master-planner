292eca85e3b49e690459503c4058602dcc3372a3
robin <drken_robin@hotmail.com>
2023-07-07 15:01:12 +0200

    remade settings to fit prod and dev

--- master_planner/master_planner/settings.py
+++ master_planner/master_planner/settings.py
@@ -18,6 +18,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 #deploy
 load_dotenv(BASE_DIR / '.env')
 
@@ -30,7 +31,7 @@ load_dotenv(BASE_DIR / '.env')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = 'RENDER' not in os.environ
 
-#ALLOWED_HOSTS = []
+
 
                                              
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -38,9 +39,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+PRODUCTION = os.getenv("PRODUCTION")
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
-
+if PRODUCTION:
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -94,17 +98,20 @@ WSGI_APPLICATION = 'master_planner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+if PRODUCTION:
+    DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
-DATABASES = {
-   'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
