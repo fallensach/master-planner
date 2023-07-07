e5798c1d06d852eef87b89ee32db6339f34b26b6
adsamu <samuelson93@gmail.com>
2023-07-07 11:05:38 +0200

    fixed reload

--- master_planner/master_planner/settings.py
+++ master_planner/master_planner/settings.py
@@ -65,8 +65,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'master_planner.urls'
