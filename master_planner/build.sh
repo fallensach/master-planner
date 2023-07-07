fd96f74313920909934ed0798a3e1e3048a43638
Robin Ngo <drken_robin@hotmail.com>
2023-07-07 14:15:02 +0200

    Update build.sh

--- master_planner/build.sh
+++ master_planner/build.sh
@@ -8,9 +8,6 @@ cd ../..
 pip install -r requirements.txt
 python manage.py tailwind build
 python manage.py collectstatic --no-input
-python manage.py sqlflush
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
 python manage.py makemigrations
 python manage.py migrate
 python manage.py populate_db
