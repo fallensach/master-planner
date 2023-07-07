8b42d29a3d8e6bf85d06cf0999b654f2c662f821
robin <drken_robin@hotmail.com>
2023-07-07 15:11:51 +0200

    Added tailwind

--- master_planner/build.sh
+++ master_planner/build.sh
@@ -6,6 +6,7 @@ npm i --production
 cd ../..
 
 pip install -r requirements.txt
+python manage.py tailwind build
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
