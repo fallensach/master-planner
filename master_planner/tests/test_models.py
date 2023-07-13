6668ad6d00abab1d31c48b12934c0a591aef9f70
adsamu <samuelson93@gmail.com>
2023-07-13 19:22:16 +0200

    API tests
    written test for most of the api using djangos built in testing module

--- master_planner/tests/test_models.py
+++ master_planner/tests/test_models.py
@@ -1,18 +1,19 @@
 from django.test import TestCase
-from django.test import Client
 from planning.models import *
 from accounts.models import *
 
-class TestApi(TestCase):
-    
-    def setUp(self):
-        user = User.objects.create_user(username="testUser",  
-                                        password="test",)
-        user.save()
-        account = Account.objects.create(user=user)
-        account.save()
+# Test all registration functions
 
-
-    def test_something(self):
-        self.client.login(username="testUser", password="123")
-        self.client.get()
+# class TestApi(TestCase):
+#     
+#     def setUp(self):
+#         user = User.objects.create_user(username="testUser",  
+#                                         password="test",)
+#         user.save()
+#         account = Account.objects.create(user=user)
+#         account.save()
+#
+#
+#     def test_something(self):
+#         self.client.login(username="testUser", password="123")
+#         self.client.get()
