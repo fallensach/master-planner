40fdb087b8a4c857afba67d6673695c2d517dbca
adsamu <samuelson93@gmail.com>
2023-07-13 10:58:44 +0200

    cleaned up filestructure for testing
    deleted test files in the apps folders and created one test folder to rule them all
    
    created a directory for scripts

--- master_planner/master_planner/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-url = "http://localhost:8000/api/get_course/1000000"
-import json
-data = json.dumps({"scheduler_id": 100})
-# url = "http://127.0.0.1:8000/api/account/choice"
-r = requests.get(url)
-
-print(r.json())
