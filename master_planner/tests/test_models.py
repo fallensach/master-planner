cf20d85c67b877c21067a7ba920b19f29722a41d
adsamu <samuelson93@gmail.com>
2023-07-14 15:23:28 +0200

    Model insertion testing
    - Test for inserting using register_programs and register_profiles have
      written.
    
    - Started working on testing register_courses

--- master_planner/tests/test_models.py
+++ master_planner/tests/test_models.py
@@ -1,19 +1,118 @@
 from django.test import TestCase
-from planning.models import *
-from accounts.models import *
+from planning.models import Scheduler, Schedule, Course, Program, Profile, MainField, Examination, register_courses, register_programs, register_profiles
+from accounts.models import User, Account
 
-# Test all registration functions
 
-# class TestApi(TestCase):
-#     
-#     def setUp(self):
-#         user = User.objects.create_user(username="testUser",  
-#                                         password="test",)
-#         user.save()
-#         account = Account.objects.create(user=user)
-#         account.save()
-#
-#
-#     def test_something(self):
-#         self.client.login(username="testUser", password="123")
-#         self.client.get()
+class TestModelsPlanning(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls): 
+        pass
+
+    def setUp(self):
+        self.program_data = [("6CMJU", 'Civilingenjörsprogram i mjukvaruteknik'), 
+                             ("6CDDD", 'Civilingenjörsprogram i datateknik')]
+
+        self.profile_data = [("profile_1", "AAAA"), 
+                             ("profile_2", "BBBB"), 
+                             ("profile_3", "CCCC")]
+        
+        self.course_data = [{"course_code": "AAAA",
+                             "course_name": "test_course_1",
+                             "hp": "6",
+                             "level": "A1X",
+                             "block": 4,
+                             "vof": "v",
+                             "profile_code": "AAAA",
+                             "period" : 1,
+                             "semester": 7
+                             },
+                            {"course_code": "BBBB",
+                             "course_name": "test_course_2",
+                             "hp": "6",
+                             "level": "A1X",
+                             "block": 4,
+                             "vof": "v",
+                             "profile_code": "AAAA",
+                             "period" : 1,
+                             "semester": 7
+                             },
+                            {"course_code": "CCCC",
+                             "course_name": "test_course_3",
+                             "hp": "6*",
+                             "level": "A1X",
+                             "block": 4,
+                             "vof": "v",
+                             "profile_code": "AAAA",
+                             "period" : 1,
+                             "semester": 7
+                             },
+                            {"course_code": "CCCC",
+                             "course_name": "test_course_3",
+                             "hp": "6*",
+                             "level": "A1X",
+                             "block": 4,
+                             "vof": "v",
+                             "profile_code": "AAAA",
+                             "period" : 1,
+                             "semester": 7
+                             }]
+        
+
+    def test_register_programs(self):
+        register_programs(self.program_data)
+        
+        programs = Program.objects.all()
+        program_count = len(self.program_data)
+
+        self.assertEqual(programs.count(), program_count)
+
+        register_programs(self.program_data)
+        
+        programs = Program.objects.all()
+
+        self.assertEqual(programs.count(), program_count)
+
+            
+    def test_register_profiles(self):
+        self.test_register_programs()
+        
+        program = Program.objects.get(program_code="6CMJU")
+        profile_count = len(self.profile_data)
+        
+        # regular insert
+        register_profiles(program, self.profile_data)
+        self.assertEqual(program.profiles.count(), profile_count)
+        
+        # double insert
+        register_profiles(program, self.profile_data)
+        self.assertEqual(program.profiles.count(), profile_count)
+        
+        # same profiles for another program
+        program = Program.objects.get(program_code="6CDDD")
+        profile_count = len(self.profile_data)
+        
+        register_profiles(program, self.profile_data)
+        self.assertEqual(Profile.objects.count(), profile_count)
+    
+    def test_register_courses(self):
+        self.test_register_programs()
+        self.test_register_profiles()
+        program = Program.objects.get(program_code="6CMJU")
+        course_instances_count = len(self.course_data)
+        register_courses(program, self.course_data)
+        print(Scheduler.objects.all())
+        self.assertEqual(Scheduler.objects.count(), course_instances_count)
+        self.assertEqual(Scheduler.objects.filter().exclude(linked=null).count(), 2)
+
+
+class TestModelsAccounts(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls): 
+        pass
+
+    def setUp(self):
+        pass
+
+
